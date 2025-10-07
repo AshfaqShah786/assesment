81dd75cb51dd121393db5e093f2c4e9e7bbf3a01
AshfaqShah786 <mhdashfaq096@gmail.com>
2025-10-07 22:28:32 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 DevOps Assessment App</h1>
+      <p>Next.js app deployed with Docker, GitHub Actions & Minikube</p>
+    </div>
+  );
+}
+
